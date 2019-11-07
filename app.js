62a995b6ff7ea4acb853a9c387a0c791411f119d
Mohamed Saqer <mohamed.saqer@outlook.com>
2019-11-07 14:14:06 +0200

    Change Location

--- app.js
+++ app.js
@@ -5,23 +5,42 @@ const weather = new Weather('cairo', 'eg', 'metric');
 const ui = new UI();
 
 // Get Weather on DOM Load
-// document.addEventListener('DOMContentLoaded', getWeather);
+// document.addEventListener('DOMContentLoaded', getWeather('metric'));
 
-// Change Location 
-// weather.changeLocation('new york', 'us');
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const country = document.getElementById('country').value;
+    let unit;
+    if (document.getElementById('unit').checked) {
+        unit = document.getElementById('unit').value;
+    }
+    if (document.getElementById('unit1').checked) {
+        unit = document.getElementById('unit1').value;
+    }
+
+    // Change Location 
+    weather.changeLocation(city, country, unit);
+
+    // Get Weather
+    getWeather(unit);
+
+    // Clear Inputs
+    document.getElementById('city').value = '';
+    document.getElementById('country').value = '';
+    document.getElementById('unit').checked = true;
+    document.getElementById('unit1').checked = false;
+
+    // Close Model
+    $('#locModal').modal('hide');
+});
 
 // Get Weather
-function getWeather(){
+function getWeather(unit){
     weather.getWeather()
         .then(results => {
             // Show UI
-            ui.paint(results, 'metric')
+            ui.paint(results, unit)
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err.message));
 }
-
-//
-document.getElementById('w-form').addEventListener('submit', (e) => {
-
-    e.preventDefault();
-});
